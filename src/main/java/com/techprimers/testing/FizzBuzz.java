a23a37f34451fd6c2e7c483202c86e198d9aea7c
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2024-04-05 11:40:07 +0530

    github test commit

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
 
     public String play(int number) {
 
-        if (number == 0) throw new IllegalArgumentException("new event for test githubhook");
+        if (number == 0) throw new IllegalArgumentException("test commit for github");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
 
